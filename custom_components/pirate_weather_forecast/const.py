af2fedcc550f47fc58ecccd9a33cc767771c1f25
slflowfoon <94804320+slflowfoon@users.noreply.github.com>
2025-09-09 21:32:42 +0100

    Update const.py

--- custom_components/pirate_weather_forecast/const.py
+++ custom_components/pirate_weather_forecast/const.py
@@ -1,15 +1,16 @@
-"""Constants for the AccuWeather Daily Forecast integration."""
+"""Constants for the Pirate Weather Daily Forecast integration."""
 from datetime import timedelta
 
-DOMAIN = "accuweather_forecast"
+DOMAIN = "pirate_weather_forecast"
 
 # Configuration constants
 CONF_API_KEY = "api_key"
-CONF_LOCATION_KEY = "location_key"
+CONF_LATITUDE = "latitude"
+CONF_LONGITUDE = "longitude"
 
 # API
-API_ENDPOINT = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}"
-ATTRIBUTION = "Data provided by AccuWeather"
+API_ENDPOINT = "https://api.pirateweather.net/forecast/{api_key}/{latitude},{longitude}"
+ATTRIBUTION = "Data provided by Pirate Weather"
 
 # Update interval
 UPDATE_INTERVAL = timedelta(minutes=60)
